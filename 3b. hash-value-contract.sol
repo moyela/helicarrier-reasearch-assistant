9501050c72e9bffa503e58e5635238d1166b53bc
Wemy Moyela <64646104+wemymoyela@users.noreply.github.com>
2021-10-29 13:39:07 +0100

    Update 3b. hash-value-contract.sol

--- 3b. hash-value-contract.sol
+++ 3b. hash-value-contract.sol
@@ -4,12 +4,8 @@ pragma solidity >=0.4.16 <0.9.0;
 contract TransactionHashStore {
 
     string question_value_hash = "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9";
-
-    function set(string question_value_hash) public {
-        question_value_hash = x;
-    }
-
-    function get() public view returns (string) {
-        return question_value_hash;
-    }
+    
+    string transaction_hash = "0xf3814e7255c1034da10689a0e703aaf3e46b2b883c90021b6be142a557b44941";
+    
 }
+//c'est fin
